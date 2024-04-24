4362132f05e7f94746754a1769b25a76a518ddb5
cristianir007 <104330258+cristianir007@users.noreply.github.com>
2024-04-24 16:41:31 -0500

    graficador.py

--- graficador.py
+++ graficador.py
@@ -38,11 +38,33 @@ class NetworkAnalysis:
     
     def read_from_file(self, filename):
         self.G = nx.read_gexf(filename)
+    
+    def build_graph_from_criteria(self, users):
+    for i in range(len(users)):
+        for j in range(i + 1, len(users)):
+            user1 = users[i]
+            user2 = users[j]
+            
+            # Criterio 1: Videojuegos favoritos compartidos
+            shared_games = set(user1["Videojuegos Favoritos"]) & set(user2["Videojuegos Favoritos"])
+            if len(shared_games) > 0:
+                self.G.add_edge(user1["ID"], user2["ID"])
+            
+            # Criterio 2: Géneros de juegos comunes
+            shared_genres = set(user1["Géneros Preferidos"]) & set(user2["Géneros Preferidos"])
+            if len(shared_genres) >= 2:  # Comparten al menos dos géneros de juegos
+                self.G.add_edge(user1["ID"], user2["ID"])
+            
+            # Criterio 3: Plataformas de juego compartidas
+            shared_platforms = set(user1["Plataformas de Juego"]) & set(user2["Plataformas de Juego"])
+            if len(shared_platforms) > 0:
+                self.G.add_edge(user1["ID"], user2["ID"])
 
 # Ejemplo de uso
 network = NetworkAnalysis()
 network.add_nodes([1, 2, 3])
 network.add_edges([(1, 2), (1, 3), (2, 3)])
+network.build_graph_from_criteria(users)
 network.visualize_graph()
 
 print("Nodos:", network.get_nodes())
